d5be8900fb611454c10a3ab04717d502e8c4b24d
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-09-26 12:31:36 -0700

    Add Prisma

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
-import { Elysia } from 'elysia';
+import { Elysia } from "elysia";
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(8080);
+const plugin = (app: Elysia) =>
+  app.state("plugin-version", 1).get("/from-plugin", () => "Hi from plugin!");
 
+type Prefix = string;
+
+const plugin2 = ({ prefix = "/v1" }: { prefix: Prefix }) =>
+  new Elysia({ prefix }).get(`/hi`, () => "Hi");
+
+const app = new Elysia()
+  .state("version", 1)
+  .decorate("getDate", () => Date.now())
+  .derive(({ request: { headers } }) => {
+    return {
+      authorization: headers.get("Authorization"),
+    };
+  })
+  .get(
+    "/",
+    ({ getDate, store: { version }, authorization }) =>
+      `version: ${version} - date: ${getDate()} - auth: ${authorization}`
+  )
+  .use(plugin)
+  .use(
+    plugin2({
+      prefix: "",
+    })
+  );
+app.listen(8080);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
